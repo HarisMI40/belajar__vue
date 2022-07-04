346f350f4f3f0df60ff6f93786a6abaa90852049
harisMI40 <harismaulanaikhsan40@gmail.com>
2022-07-04 14:53:00 +0700

    versi pertama, program ganti foto

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
- <h1>{{nama}}</h1>
- <img :src="foto[0].url"/>
- <button v-on:click="updateName">Change Name</button>
+ <h1>{{angka}}</h1>
+ <div>
+   <img :src="foto" width="500"/>
+ </div>
+ <button v-on:click="changeImage">Change Image</button>
 </template>
 
 <script>
+const fotoss = [
+     {
+      id: 1, 
+      url : "https://media.suara.com/pictures/653x366/2021/12/28/11513-ilustrasi-kucing-dengan-mata-beda-warna-pixabaybaran65.jpg"
+      },
+      {
+        id: 2, 
+        url : "https://cdn1-production-images-kly.akamaized.net/nQF66ic6xRDS2tL6yNU3nBrFBqQ=/1200x1200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3209208/original/001184800_1597393099-kucingjahilcov.jpg"
+      },
+      {
+        id: 3, 
+        url : "https://www.ruparupa.com/blog/wp-content/uploads/2021/02/Screen-Shot-2021-02-05-at-11.53.27-1024x622.png"
+      }
+];
+
+var angka = 0;
 
 export default {
   name: 'App',
@@ -13,14 +31,20 @@ export default {
   data() {
     return{
       nama : " ini nama",
-      foto : [
-        {id: 1, url : "https://media.suara.com/pictures/653x366/2021/12/28/11513-ilustrasi-kucing-dengan-mata-beda-warna-pixabaybaran65.jpg"}
-      ]
+      angka : 0,
+      foto : fotoss[angka].url
     }
   },
   methods : {
-    updateName(){
-      this.nama = "update name"
+    changeImage(){
+      if(this.angka < 2){
+        this.foto = fotoss[this.angka + 1].url;
+        this.angka = this.angka + 1;
+        
+      }else{
+        this.angka = 0;
+        this.foto = fotoss[this.angka].url;
+      }
     }
   }
 }
@@ -34,5 +58,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img{
+    background-size: cover;
+    height: 500;
+    height: 304px;
+    background-position: center;
 }
 </style>
